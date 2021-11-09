5c5f189462f60246cf7a46bd258deba4c06921ad
Olav Hartwedt Larsen <ohalar@gmail.com>
2021-11-09 10:58:13 +0100

    Removed unused code

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -8,36 +8,6 @@ const Home = () => {
       <PlayerList />
     </div>
   );
-
-  // const [players, setPlayers] = useState<IPlayer[]>([
-  //   { id: "jup", firstname: "Ola", lastname: "Nordmann" },
-  // ]);
-
-  // useEffect(() => {
-  //   getPlayers();
-  // }, []);
-
-  // const getPlayers = async () => {
-  //   const result = await playerService.getAllPlayers();
-  //   setPlayers(result);
-  // };
-
-  // const createPlayerList = () => {
-  //   return players.map((player: IPlayer, key: number) => {
-  //     return (
-  //       <h4 key={key}>
-  //         {player.firstname} {player.lastname}
-  //       </h4>
-  //     );
-  //   });
-  // };
-
-  // return (
-  //   <div>
-  //     <h1>Liverpool jup</h1>
-  //     {createPlayerList()}
-  //   </div>
-  // );
 };
 
 export default Home;
