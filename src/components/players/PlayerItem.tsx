c90ca5c0217f35509f8d8a3a5007a69936a75c93
Olav Hartwedt Larsen <ohalar@gmail.com>
2021-11-10 12:14:59 +0100

    Made Modal for each player

--- src/components/players/PlayerItem.tsx
+++ src/components/players/PlayerItem.tsx
@@ -1,8 +1,17 @@
+import Button from "@restart/ui/esm/Button";
 import { FC } from "react";
 import { Card } from "react-bootstrap";
 import { IPlayer } from "../../interfaces/IPlayers";
 
-const PlayerItem: FC<IPlayer> = (player: IPlayer) => {
+type PlayerProps = {
+  player: IPlayer;
+  handleClick: (player: IPlayer) => void;
+};
+
+const PlayerItem: FC<PlayerProps> = ({ player, handleClick }: PlayerProps) => {
+  const clickedElement = (player: IPlayer) => {
+    handleClick(player);
+  };
   return (
     <Card>
       <Card.Body>
@@ -14,6 +23,9 @@ const PlayerItem: FC<IPlayer> = (player: IPlayer) => {
         <Card.Title>
           {player.firstname} {player.lastname}
         </Card.Title>
+        <Button type="button" onClick={() => clickedElement(player)}>
+          OK
+        </Button>
       </Card.Body>
     </Card>
   );
--- src/components/players/PlayerList.tsx
+++ src/components/players/PlayerList.tsx
@@ -1,5 +1,5 @@
-import { FC, useContext } from "react";
-import { Col, Row } from "react-bootstrap";
+import { FC, useContext, useState } from "react";
+import { Col, Modal, Row } from "react-bootstrap";
 import { PlayerContext } from "../../context/PlayerContext";
 import { IPlayer } from "../../interfaces/IPlayers";
 import { PlayerContextType } from "../../types/PlayerContextType";
@@ -7,6 +7,55 @@ import PlayerItem from "./PlayerItem";
 
 const PlayerList: FC = () => {
   const { players } = useContext(PlayerContext) as PlayerContextType;
+  const [showModal, setShowModal] = useState(false);
+  const [selectedPlayer, setSelectedPlayer] = useState<IPlayer>({
+    firstname: "",
+    lastname: "",
+    age: 0,
+    country: "",
+    position: "",
+    image: "",
+  });
+
+  const handleShowModal = () => setShowModal(true);
+  const handleCloseModal = () => setShowModal(false);
+
+  const handleClick = (player: IPlayer) => {
+    setSelectedPlayer(player);
+    setShowModal(true);
+  };
+
+  const displayModal = (player: IPlayer) => {
+    console.log("OK");
+    return (
+      <Modal show={showModal} onHide={handleCloseModal}>
+        <Modal.Header closeButton>
+          <Modal.Title>
+            {player.firstname} {player.lastname}
+          </Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <img
+            src={`https://localhost:5001/images/${player.image}`}
+            alt={player.firstname}
+          />
+          <div>
+            <label>Name</label>
+            <input
+              type="text"
+              value={`${player.firstname} ${player.lastname}`}
+            />
+            <label>Age</label>
+            <input type="text" value={player.age} />
+            <label>Country</label>
+            <input type="text" value={player.country} />
+            <label>Position</label>
+            <input type="text" value={player.position} />
+          </div>
+        </Modal.Body>
+      </Modal>
+    );
+  };
 
   const displayPlayers = () => {
     console.log(players);
@@ -14,13 +63,18 @@ const PlayerList: FC = () => {
     return players.map((player: IPlayer, key) => {
       return (
         <Col key={key} md="6" lg="4" xl="3">
-          <PlayerItem {...player} />
+          <PlayerItem player={player} handleClick={handleClick} />
         </Col>
       );
     });
   };
 
-  return <Row>{displayPlayers()}</Row>;
+  return (
+    <>
+      <Row>{displayPlayers()}</Row>
+      {showModal && displayModal(selectedPlayer)}
+    </>
+  );
 };
 
 export default PlayerList;
