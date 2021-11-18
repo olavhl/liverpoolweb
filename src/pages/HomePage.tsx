214692cf78cce55b9b4b9974255bd47159b01672
Olav Hartwedt Larsen <ohalar@gmail.com>
2021-11-18 17:08:50 +0100

    Displaying random player on homepage with animation

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,31 +1,36 @@
-import {useContext, useEffect, useState} from "react";
+import {useContext, useState} from "react";
 import {PlayerContext} from "../context/PlayerContext";
 import {PlayerContextType} from "../types/PlayerContextType";
 import PlayerItem from "../components/players/list/PlayerItem";
 import {IPlayer} from "../interfaces/IPlayers";
+import {Button} from "react-bootstrap";
 
 const Home = () => {
     const {players} = useContext(PlayerContext) as PlayerContextType;
     const [randomPlayer, setRandomPlayer] = useState<IPlayer>()
+    const [faded, setFaded] = useState(false)
 
-    useEffect(() => {
-        generateRandomPlayer()
-    }, [players])
+    // Creating variables to be able to use animations
+    const fade = faded ? 'fade' : '';
+    const classes = `random-player ${fade}`
 
     const generateRandomPlayer = () => {
         const num = Math.floor(Math.random() * players.length);
         let playersCopy = [...players];
         setRandomPlayer(playersCopy[num])
+        setFaded(true)
     }
 
   return (
-    <div>
+    <div className={"home-container"}>
       <h1>Get a random player</h1>
+        <Button className={"random-player-btn"} onClick={generateRandomPlayer}>Show Random Player</Button>
         {randomPlayer &&
-        <div className={"random-player"}>
+        <div className={classes}>
             <PlayerItem player={randomPlayer}/>
         </div>
         }
+
     </div>
   );
 };
